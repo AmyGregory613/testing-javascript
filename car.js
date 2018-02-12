b94b5234566fa795cd9418dcaf31765c45efc8c2
Amy Gregory <aegregory46@gmail.com>
2018-02-12 14:23:22 -0600

    driveForSomeTime with tests/ not working/ car undefined message after it had been working

--- car.js
+++ car.js
@@ -9,14 +9,16 @@ function Car (make, model, year) {
 	this.mileage = 0;
 	this.drive = function(milesCarShouldDrive) {
       if (!milesCarShouldDrive || milesCarShouldDrive <= 0)
-        throw new Error("missing mileage");
+        throw new Error("mileage error");
     	this.mileage += milesCarShouldDrive;
+    	return this.mileage;
     }
 }
 
 // Add a driveForSomeTime function that takes the amount 
 // of seconds to drive for and a number representing the speed 
 // to drive at in miles per second (yeah I know, that's fast).
+
 //   -If seconds is negative, zero or no value, an Error is thrown.
 //   - If Miles Per Second is negative, zero or no value, 
 // an error is thrown.
@@ -24,14 +26,28 @@ function Car (make, model, year) {
 // The function should increment the mileage by the given 
 // amount every second until the given amount of time has elapsed.
 
+		// Increment seconds counter by 1
+		// call drive (remember, while you're in the interval function "self" 
+		// is an alias for "this" so use it instead of "this"
+		
+     
 
-    this.drive = function(driveForSomeTime) {
-      setTimeout(function(self) {
-        try {
-          if (!driveForSomeTime || driveForSomeTime <= 0)
+    this.driveForSomeTime = function (secondsToDriveFor, milesPerSecond) {
+     // Make sure secondsToDriveFor and milesPerSecond are valid. 
+     // Create variable to keep track of how many seconds have elapsed.
+      setInterval(function(self) {
+          if (!secondsToDriveFor || secondsToDriveFor <= 0)
         throw new Error("Time Value Error");
+        
+        if (!milesPerSecond || milesPerSecond <= 0)
+        throw new Error("Speed Value Error");
         }
+        var timeDriven = 0;
+        timeDriven += 1000;
+
+        // If the requested number of seconds have elapsed, stop interval
       }, 1000, this);
+    
     }
   }
   exports.Car = Car;
--- car_test.js
+++ car_test.js
@@ -3,7 +3,7 @@ var expect = chai.expect;
 describe("Car", function() {
   describe("constructor", function() {
     it("should have a default year if not provided", function() {
-      var car = new Car();
+      var car = new Car("Subaru", "Forester");
       expect(car.year).to.equal("2018");
     });
 
@@ -22,7 +22,7 @@ describe("Car", function() {
       expect(car.model).to.equal("Forester");
     });
   it("should begin with mileage of 0", function() {
-      var car = new Car();
+      var car = new Car("Subaru", "Forester", "2012");
       expect(car.mileage).to.equal(0);
     });
 });
@@ -52,3 +52,55 @@ describe("Car", function() {
   });
 
 });
+// Drive For Some Given Time Tests:
+//     Verify that no seconds throws an Error
+describe("#driveForSomeTime", function() {
+    it("should throw if no time is passed in", function() {
+      expect(function() {
+        (new Car("Subaru", "Forester", "2012")).driveForSomeTime();
+      }).to.throw(Error);
+    });
+    //     Verify that zero seconds throws an Error
+    it("should throw if mileage passed in = 0", function() {
+      expect(function() {
+        (new Car("Subaru", "Forester", "2012")).driveForSomeTime(0);
+      }).to.throw(Error);
+    });
+    //     Verify that negative seconds throws an Error
+    it("should throw if mileage passed in < 0", function() {
+      expect(function() {
+        (new Car("Subaru", "Forester", "2012")).driveForSomeTime(-1);
+      }).to.throw(Error);
+    });
+
+//     Verify that no miles/second throws an Error
+    it("should throw if no time is passed in", function() {
+      expect(function() {
+        (new Car("Subaru", "Forester", "2012")).drive().driveForSomeTime();
+      }).to.throw(Error);
+    });
+    //     Verify that zero miles/second throws an Error
+    it("should throw if mileage passed in = 0", function() {
+      expect(function() {
+        (new Car("Subaru", "Forester", "2012")).drive(0).driveForSomeTime(0);
+      }).to.throw(Error);
+    });
+    //     Verify that negative miles/second throws an Error
+    it("should throw if mileage passed in < 0", function() {
+      expect(function() {
+        (new Car("Subaru", "Forester", "2012")).drive(-1).driveForSomeTime(-1);
+      }).to.throw(Error);
+    });
+//     Verify that positive miles/second then the car drives the appropriate amount of miles.
+    it("should drive X number of miles/second", function() {
+      var car = new Car("Subaru", "Forester", "2012");
+      car.milesPerSecond(50);
+      expect(car.milesPerSecond).to.equal(50);
+    });
+
+  });
+
+
+
+
+
